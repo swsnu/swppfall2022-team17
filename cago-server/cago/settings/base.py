e11cc40595cacee14b652db28785baaf1e8348d9
askrid <joonwoo3023@gmail.com>
2022-10-25 21:40:20 +0900

    use Django static files

--- cago-server/cago/settings/base.py
+++ cago-server/cago/settings/base.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from decouple import config
@@ -82,6 +83,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
