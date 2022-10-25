cff276a86f6597e86586571620e0bfb494bb744c
askrid <joonwoo3023@gmail.com>
2022-10-25 21:40:50 +0900

    make server script synchronous

--- cago-server/start-server.sh
+++ cago-server/start-server.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-(python3 manage.py migrate) &
-(gunicorn cago.wsgi --bind 127.0.0.1:8080 --workers 3) &
-nginx -g "daemon off;"
+python3 manage.py migrate
+nginx
+gunicorn cago.wsgi --bind 127.0.0.1:8080 --workers 3
