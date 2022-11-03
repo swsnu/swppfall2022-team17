f5b6a3ea9556315f77f7160433265c8711133530
askrid <joonwoo3023@gmail.com>
2022-11-03 23:12:05 +0900

    server: set namespace for 'ping' app

--- cago-server/cago/ping/urls.py
+++ cago-server/cago/ping/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+app_name = "ping"
+
 urlpatterns = [
-    path("", views.PingAPIView.as_view(), name="ping"),
+    path("", views.PingAPIView.as_view(), name="index"),
 ]
