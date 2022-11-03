011d69e3d3a290d69b4f69d900e29d5861aa0bda
askrid <joonwoo3023@gmail.com>
2022-11-04 01:26:58 +0900

    server: implement signup API

--- cago-server/cago/user/urls.py
+++ cago-server/cago/user/urls.py
@@ -5,10 +5,13 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
+from . import views
+
 app_name = "user"
 
 urlpatterns = [
     path("login/", TokenObtainPairView.as_view(), name="login"),
+    path("signup/", views.SignUpView.as_view(), name="signup"),
     path("refresh/", TokenRefreshView.as_view(), name="refresh"),
     path("verify/", TokenVerifyView.as_view(), name="verify"),
 ]
