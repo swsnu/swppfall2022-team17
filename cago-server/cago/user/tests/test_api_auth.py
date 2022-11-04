8d23cf1831cd007acea5d6534a47628b7ccde6aa
askrid <joonwoo3023@gmail.com>
2022-11-04 19:24:02 +0900

    server: test auth APIs

---
+++ cago-server/cago/user/tests/test_api_auth.py
@@ -0,0 +1,140 @@
+from django.contrib.auth import get_user_model
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from rest_framework_simplejwt.tokens import AccessToken, RefreshToken
+
+User = get_user_model()
+
+
+class LoginAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_email = "test1@test.com"
+        cls.user_password = "qwer1234"
+        cls.user = User.objects.create_user(
+            email=cls.user_email, password=cls.user_password
+        )
+
+    def test_login_success(self):
+        response = self.client.post(
+            reverse("auth:login"),
+            {"email": self.user_email, "password": self.user_password},
+        )
+        self.assertContains(response, "access")
+
+        data = response.json()
+        access_token = AccessToken(data.get("access"))
+        refresh_token = RefreshToken(data.get("refresh"))
+        self.assertEqual(access_token.get("user_id"), self.user.id)
+        self.assertEqual(refresh_token.get("user_id"), self.user.id)
+
+    def test_after_login(self):
+        access_token = AccessToken.for_user(self.user)
+        response = self.client.get(
+            reverse("ping:index"), HTTP_AUTHORIZATION="Bearer " + str(access_token)
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_login_invalid_credentials(self):
+        response = self.client.post(
+            reverse("auth:login"),
+            {"email": self.user_email, "password": self.user_password + "0"},
+        )
+        self.assertContains(
+            response, "no_active_account", status_code=status.HTTP_401_UNAUTHORIZED
+        )
+
+
+class SignUpAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user_email = "test1@test.com"
+        cls.user_password = "qwer1234"
+
+    def test_signup_success(self):
+        response = self.client.post(
+            reverse("auth:signup"),
+            {
+                "email": self.user_email,
+                "password": self.user_password,
+                "password_confirm": self.user_password,
+            },
+        )
+        self.assertContains(
+            response, self.user_email, status_code=status.HTTP_201_CREATED
+        )
+
+    def test_signup_invalid_password_confirm(self):
+        response = self.client.post(
+            reverse("auth:signup"),
+            {
+                "email": self.user_email,
+                "password": self.user_password,
+                "password_confirm": self.user_password + "0",
+            },
+        )
+        self.assertContains(
+            response, "password_confirm", status_code=status.HTTP_400_BAD_REQUEST
+        )
+
+    def test_signup_duplciate_email(self):
+        User.objects.create_user(email=self.user_email, password=self.user_password)
+        response = self.client.post(
+            reverse("auth:signup"),
+            {
+                "email": self.user_email,
+                "password": self.user_password,
+                "password_confirm": self.user_password,
+            },
+        )
+        self.assertContains(response, "email", status_code=status.HTTP_400_BAD_REQUEST)
+
+
+class RefrehAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(email="test1@test.com", password="qwer1234")
+        cls.refresh_token = RefreshToken.for_user(cls.user)
+
+    def test_refresh_success(self):
+        response = self.client.post(
+            reverse("auth:refresh"), {"refresh": str(self.refresh_token)}
+        )
+        self.assertContains(response, "access")
+
+    def test_refresh_invalid_token(self):
+        response = self.client.post(
+            reverse("auth:refresh"), {"refresh": str(self.refresh_token) + "0"}
+        )
+        self.assertContains(
+            response, "token_not_valid", status_code=status.HTTP_401_UNAUTHORIZED
+        )
+
+
+class VerifyAPITestCase(APITestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.user = User.objects.create_user(email="test1@test.com", password="qwer1234")
+        cls.access_token = AccessToken.for_user(cls.user)
+        cls.refresh_token = RefreshToken.for_user(cls.user)
+
+    def test_verify_access_success(self):
+        response = self.client.post(
+            reverse("auth:verify"), {"token": str(self.access_token)}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_verify_refresh_success(self):
+        response = self.client.post(
+            reverse("auth:verify"), {"token": str(self.refresh_token)}
+        )
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_verify_invalid_token(self):
+        response = self.client.post(
+            reverse("auth:verify"), {"token": str(self.access_token) + "0"}
+        )
+        self.assertContains(
+            response, "token_not_valid", status_code=status.HTTP_401_UNAUTHORIZED
+        )
