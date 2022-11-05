0d0dcb6656d3fdc55983a05ca11f351e5b98536c
askrid <joonwoo3023@gmail.com>
2022-11-06 07:36:14 +0900

    server: create logout API

--- cago-server/cago/user/urls.py
+++ cago-server/cago/user/urls.py
@@ -6,6 +6,7 @@ app_name = "user"
 
 urlpatterns = [
     path("login/", views.LoginView.as_view(), name="login"),
+    path("logout/", views.LogoutView.as_view(), name="logout"),
     path("signup/", views.SignUpView.as_view(), name="signup"),
     path("refresh/", views.CookieTokenRefreshView.as_view(), name="refresh"),
     path("verify/", views.CookieTokenVerifyView.as_view(), name="verify"),
