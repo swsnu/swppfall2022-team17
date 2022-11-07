585140c3ffd80471ba237177cebc9c15f57e4fda
askrid <joonwoo3023@gmail.com>
2022-11-07 21:16:25 +0900

    web: create Cago header

--- cago-web/src/pages/cafes/index.tsx
+++ cago-web/src/pages/cafes/index.tsx
@@ -1,14 +1,21 @@
+import CagoHeader from "components/layouts/CagoHeader";
 import Container from "components/layouts/Container";
 import { NextPageWithLayout } from "pages/_app";
 
 const Cafes: NextPageWithLayout = () => {
   return (
     <main>
-      <h1>Cafes</h1>
+      <h1 className="text-center italic">(Cafes)</h1>
+      <div className="h-screen" />
     </main>
   );
 };
 
-Cafes.getLayout = (page) => <Container>{page}</Container>;
+Cafes.getLayout = (page) => (
+  <>
+    <CagoHeader />
+    <Container>{page}</Container>
+  </>
+);
 
 export default Cafes;
