1993d91152e1b0a2ef7ce04e719d2e17cfe78a13
askrid <joonwoo3023@gmail.com>
2022-11-09 06:02:52 +0900

    server: create cafe related models

--- cago-server/cago/cafe/models.py
+++ cago-server/cago/cafe/models.py
@@ -1,3 +1,114 @@
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.core.validators import MaxValueValidator
 from django.db import models
+from django.utils.text import slugify
+from phonenumber_field.modelfields import PhoneNumberField
 
-# Create your models here.
+User = get_user_model()
+
+default_avatar = "https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678099-profile-filled-512.png"
+default_menu_image = (
+    "https://cdn4.iconfinder.com/data/icons/sketchy-basic-icons/94/coffee-512.png"
+)
+
+
+class CustomerProfile(models.Model):
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, related_name="customer_profile"
+    )
+    display_name = models.CharField(max_length=64)
+    avatar = models.URLField(max_length=256, default=default_avatar)
+
+    class Meta:
+        verbose_name = "customer_profile"
+        verbose_name_plural = "customer_profiles"
+
+
+class Cafe(models.Model):
+    business_name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128, unique=True)
+    phone_number = PhoneNumberField()
+    # TODO: Use Google map API's geocoding (https://developers.google.com/maps/documentation/geocoding/overview)
+    # location = PointField()
+    # address = models.CharField(max_length=256)
+
+    class Meta:
+        verbose_name = "cafe"
+        verbose_name_plural = "cafes"
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.slug)
+        super().save(*args, **kwargs)
+
+    @property
+    def is_managed(self):
+        if hasattr(self, "managedcafe"):
+            return True
+
+        return False
+
+
+class ManagedCafe(Cafe):
+    class Crowdedness(models.IntegerChoices):
+        UNKNOWN = 0
+        LOW = 1
+        MIDDLE = 2
+        HIGH = 3
+
+    registration_number = models.PositiveBigIntegerField(
+        validators=[MaxValueValidator(9999999999)]
+    )
+    representative = models.ForeignKey(
+        User, related_name="owned_cafes", on_delete=models.CASCADE
+    )
+    managers = models.ManyToManyField(User, related_name="managed_cafes")
+    display_name = models.CharField(max_length=64)
+    avatar = models.URLField(max_length=256, default=default_avatar)
+    force_closed = models.BooleanField(default=False)
+    crowdedness = models.IntegerField(
+        choices=Crowdedness.choices, default=Crowdedness.UNKNOWN
+    )
+
+    class Meta:
+        verbose_name = "managed_cafe"
+        verbose_name_plural = "managed_cafes"
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        super().save(*args, **kwargs)
+
+    def validate_unique(self, *args, **kwargs):
+        queryset = ManagedCafe.objects.filter(
+            registration_number=self.registration_number,
+            business_name=self.business_name,
+        )
+        if queryset.exists():
+            raise ValidationError("cafe already exists")
+
+        return super().validate_unique(*args, **kwargs)
+
+
+class CafeImage(models.Model):
+    cafe = models.ForeignKey(Cafe, related_name="images", on_delete=models.CASCADE)
+    url = models.URLField(max_length=256)
+    is_main = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name = "cafe_image"
+        verbose_name_plural = "cafe_images"
+
+        constraints = [
+            models.UniqueConstraint(
+                fields=["cafe", "is_main"],
+                condition=models.Q(is_main=True),
+                name="unique_main_image",
+            )
+        ]
+
+
+class CafeMenu(models.Model):
+    cafe = models.ForeignKey(Cafe, related_name="menus", on_delete=models.CASCADE)
+    name = models.CharField(max_length=64)
+    price = models.PositiveIntegerField()
+    image = models.URLField(max_length=256, default=default_menu_image)
