3a8f0f0f0dbd0220043df2631e4770f32f896ca9
askrid <joonwoo3023@gmail.com>
2022-11-08 22:03:08 +0900

    web: use Korean in register cafe page

--- cago-web/src/pages/admin/register-cafe.tsx
+++ cago-web/src/pages/admin/register-cafe.tsx
@@ -7,10 +7,8 @@ import { NextPageWithLayout } from "pages/_app";
 const RegisterCafe: NextPageWithLayout = () => {
   return (
     <main className="flex flex-col h-screen items-center justify-center">
-      <h1 className="font-bold text-4xl mb-8">Register Your Cafe</h1>
-      <h2 className="font-semibold text-2xl mb-8">
-        Fill in the form below based on your business license
-      </h2>
+      <h1 className="font-bold text-4xl mb-8">카페 등록하기</h1>
+      <h2 className="font-semibold text-2xl mb-8">사업자 등록증에 따라 아래 양식을 작성하세요</h2>
       <div className="md:w-96 w-full">
         <div className="mb-4">
           <RegisterCafeForm />
