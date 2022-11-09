d8fe2421097c77ca885a9d072bfdc0568a07e41d
ImPJH <impjhy1129@naver.com>
2022-11-10 03:01:40 +0900

    web: change Cafe introduction look

--- cago-web/src/components/forms/CafeStatisticsForm.tsx
+++ cago-web/src/components/forms/CafeStatisticsForm.tsx
@@ -11,18 +11,24 @@ const CafeStatisticForm: NextComponentType = () => {
       <div className="border-b-2 border-black w-full mb-2">
         Cafe Introduction
       </div>
-      <div className="flex h-full text-center align-middle">
-        <div className="flex flex-col border border-solid border-black rounded w-2/6 mr-2">
-            {id && dashboardData[parseInt(id)-1].best}
-            <div className="text-3xl">Best</div>
+      <div className="flex h-full text-center align-middle place">
+        <div className="flex flex-col border border-solid border-black rounded w-2/6 mr-2 place-content-evenly">
+          <div className="text-sm font-bold">
+            {id && dashboardData[parseInt(id) - 1].best}
+          </div>
+          <div className="text-3xl">Best</div>
         </div>
-        <div className="flex flex-col border border-solid border-black rounded w-2/6 mx-2">
-            {id && dashboardData[parseInt(id)-1].reviews}
-            <div className="text-3xl">Reviews</div>
+        <div className="flex flex-col border border-solid border-black rounded w-2/6 mx-2 place-content-evenly">
+          <div className="text-sm font-bold">
+            {id && dashboardData[parseInt(id) - 1].reviews}
+          </div>
+          <div className="text-3xl">Reviews</div>
         </div>
-        <div className="flex flex-col border border-solid border-black rounded w-2/6 ml-2">
-            {id && dashboardData[parseInt(id)-1].likes}
-            <div className="text-3xl">Likes</div>
+        <div className="flex flex-col border border-solid border-black rounded w-2/6 ml-2 place-content-evenly">
+          <div className="text-sm font-bold">
+            {id && dashboardData[parseInt(id) - 1].likes}
+          </div>
+          <div className="text-3xl">Likes</div>
         </div>
       </div>
     </div>
