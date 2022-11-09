f3ce53a572cb0896a81ffc67357b121aec6d956a
ImPJH <impjhy1129@naver.com>
2022-11-10 03:20:04 +0900

    web: delete test message

--- cago-web/src/pages/admin/dashboard/[id]/info.tsx
+++ cago-web/src/pages/admin/dashboard/[id]/info.tsx
@@ -1,29 +1,27 @@
-import Container from "components/layouts/Container"
-import { NextPageWithLayout } from "pages/_app"
-import { useRouter } from 'next/router'
-import CagoAdminHeader from "components/layouts/CagoAdminHeader"
-import CafeIntroductionForm from "components/forms/CafeIntroductionForm"
-import CafeStatisticForm from "components/forms/CafeStatisticsForm"
+import Container from "components/layouts/Container";
+import { NextPageWithLayout } from "pages/_app";
+import CagoAdminHeader from "components/layouts/CagoAdminHeader";
+import CafeIntroductionForm from "components/forms/CafeIntroductionForm";
+import CafeStatisticForm from "components/forms/CafeStatisticsForm";
+import BusinessIntroductionForm from "components/forms/BusinessIntroductionForm";
 
 const Info: NextPageWithLayout = () => {
-    const router = useRouter()
-    const { id } = router.query
-    return (
-        <>
-            <CafeIntroductionForm/>
-            <CafeStatisticForm />
-            HI! This is for test for info page! And This is your Cafe ID : {id}
-        </>
-    )
-}
+  return (
+    <>
+      <CafeStatisticForm />
+      <CafeIntroductionForm />
+      <BusinessIntroductionForm />
+    </>
+  );
+};
 
 Info.getLayout = (page) => {
-    return (
-        <>
-            <CagoAdminHeader />
-            <Container>{page}</Container>
-        </>
-    )
-}
+  return (
+    <>
+      <CagoAdminHeader />
+      <Container>{page}</Container>
+    </>
+  );
+};
 
-export default Info
+export default Info;
