951b546d759f3f8fec08d557b470c271db877d6f
ImPJH <impjhy1129@naver.com>
2022-11-10 04:25:39 +0900

    web: change css

--- cago-web/src/components/contents/BusinessIntroduction.tsx
+++ cago-web/src/components/contents/BusinessIntroduction.tsx
@@ -7,8 +7,8 @@ const BusinessIntroduction: NextComponentType = () => {
   const router = useRouter();
   const id = router.query.id as string | undefined;
   return (
-    <div className="flex flex-col justify-center h-96 mt-2">
-      <div className="border-b-2 border-black w-full mb-2">
+    <div className="flex flex-col justify-center h-72 mt-2">
+      <div className="text-2xl font-bold border-b-2 border-black w-full mb-2 shadow">
         Business Introduction
       </div>
       <div className="border border-solid border-black rounded h-full text-center align-middle mb-2">
