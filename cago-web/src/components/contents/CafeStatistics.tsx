c8c1b6abf8e988289cb155eeb41b6c349b6cd4ce
ImPJH <impjhy1129@naver.com>
2022-11-10 06:27:07 +0900

    web: create StatisticBox method

--- cago-web/src/components/contents/CafeStatistics.tsx
+++ cago-web/src/components/contents/CafeStatistics.tsx
@@ -7,21 +7,27 @@ import ComponentContainer from "components/contents/ComponentContainer";
 const CafeStatistic: NextComponentType = () => {
   const router = useRouter();
   const id = router.query.id as string | undefined;
+
+  interface Props {
+    dataName: string;
+    data: number | string | undefined;
+  }
+
+  const StatisticBox = ({dataName,data}:Props) => {
+    return (
+      <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly py-4">
+        <div className="text-3xl">{dataName}</div>
+        <div>{data}</div>
+      </div>
+    );
+  };
+  
   return (
     <ComponentContainer title="Cafe Statistics">
       <div className="flex h-full text-center align-middle place mb-2">
-        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly py-4">
-          <div className="text-3xl">Best</div>
-          <div>{id && dashboardData[parseInt(id) - 1].best}</div>
-        </div>
-        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly py-4">
-          <div className="text-3xl">Reviews</div>
-          <div>{id && dashboardData[parseInt(id) - 1].reviews}</div>
-        </div>
-        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly py-4">
-          <div className="text-3xl">Likes</div>
-          <div>{id && dashboardData[parseInt(id) - 1].likes}</div>
-        </div>
+        <StatisticBox dataName="Best" data={id && dashboardData[parseInt(id) - 1].best}/>
+        <StatisticBox dataName="Reviews" data={id && dashboardData[parseInt(id) - 1].reviews}/>
+        <StatisticBox dataName="Likes" data={id && dashboardData[parseInt(id) - 1].likes}/>
       </div>
     </ComponentContainer>
   );
