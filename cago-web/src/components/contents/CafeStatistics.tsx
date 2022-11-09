a5a80eb01737d3692b312f5830fa815a258de0c5
ImPJH <impjhy1129@naver.com>
2022-11-10 06:17:22 +0900

    web: create ComponentContainer

--- cago-web/src/components/contents/CafeStatistics.tsx
+++ cago-web/src/components/contents/CafeStatistics.tsx
@@ -2,30 +2,28 @@ import { NextComponentType } from "next";
 import { useRouter } from "next/router";
 import React from "react";
 import dashboardData from "data/dashboard.json";
+import ComponentContainer from "components/contents/ComponentContainer";
 
 const CafeStatistic: NextComponentType = () => {
   const router = useRouter();
   const id = router.query.id as string | undefined;
   return (
-    <div className="flex flex-col justify-center h-36 mt-2">
-      <div className="text-2xl font-bold w-full mb-4">
-        Cafe Statistics
-      </div>
+    <ComponentContainer title="Cafe Statistics">
       <div className="flex h-full text-center align-middle place mb-2">
-        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly">
+        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly py-4">
           <div className="text-3xl">Best</div>
           <div>{id && dashboardData[parseInt(id) - 1].best}</div>
         </div>
-        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly">
+        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly py-4">
           <div className="text-3xl">Reviews</div>
           <div>{id && dashboardData[parseInt(id) - 1].reviews}</div>
         </div>
-        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly">
+        <div className="bg-slate-50 shadow-lg flex flex-col rounded w-2/6 mr-2 place-content-evenly py-4">
           <div className="text-3xl">Likes</div>
           <div>{id && dashboardData[parseInt(id) - 1].likes}</div>
         </div>
       </div>
-    </div>
+    </ComponentContainer>
   );
 };
 
---
+++ cago-web/src/components/contents/ComponentContainer.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+interface Props {
+    title:string
+    children?: React.ReactNode;
+}
+
+const ComponentContainer = ({title, children}: Props)=>{
+    return(
+        <div className="flex flex-col justify-center max-h-[60vh] my-4">
+            <div className="text-2xl font-bold w-full mb-4">
+                {title}
+            </div>
+            <div className="max-h-[50vh] overflow-y">
+                {children}
+            </div>
+        </div>
+        )
+}
+
+export default ComponentContainer;
