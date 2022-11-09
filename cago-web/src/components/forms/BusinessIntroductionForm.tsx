42f17984d7152cdd2193add627bd76def566cc83
ImPJH <impjhy1129@naver.com>
2022-11-10 03:22:59 +0900

    web: create Business Introduction Form

--- cago-web/src/components/forms/BusinessIntroductionForm.tsx
+++ cago-web/src/components/forms/BusinessIntroductionForm.tsx
@@ -1,24 +1,24 @@
-
 import { NextComponentType } from "next";
 import { useRouter } from 'next/router'
 import React from "react";
 import dashboardData from "data/dashboard.json";
 
 
-const CafeIntroductionForm: NextComponentType = () => {
+
+const BusinessIntroductionFormForm: NextComponentType = () => {
     const router = useRouter()
     const id = router.query.id as string | undefined;
     return (
         <div className="flex flex-col justify-center h-96 mt-2">
             <div className="border-b-2 border-black w-full mb-2">
-                Cafe Introduction
+                Business Introduction
             </div>
             <div className="border border-solid border-black rounded h-full text-center align-middle">
-                {id && dashboardData[parseInt(id)-1].introduction}
+                Business Introduction Detail
             </div>
         </div>
     )
 }
 
 
-export default CafeIntroductionForm;
+export default BusinessIntroductionFormForm;
