6d6c1050aef73db39aab2b32d40abce14ebe1146
ImPJH <impjhy1129@naver.com>
2022-11-10 08:01:40 +0900

    web: Create Cafe Info Page

--- cago-web/src/pages/cafes/[id]/info.tsx
+++ cago-web/src/pages/cafes/[id]/info.tsx
@@ -2,13 +2,17 @@ import CafeButtonGroup from "components/layouts/CafeButtonGroup";
 import CagoHeader from "components/layouts/CagoHeader";
 import Container from "components/layouts/Container";
 import { NextPageWithLayout } from "pages/_app";
+import CafeIntroduction from "components/contents/CafeIntroduction";
+import CafeStatistic from "components/contents/CafeStatistics";
+import BusinessInformation from "components/contents/BusinessInformation";
 
 const Info: NextPageWithLayout = () => {
   return (
-    <main>
-      <h1 className="text-center italic">(Info)</h1>
-      <div className="h-screen" />
-    </main>
+    <>
+      <CafeStatistic />
+      <CafeIntroduction />
+      <BusinessInformation />
+    </>
   );
 };
 
