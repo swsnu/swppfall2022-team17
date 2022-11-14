3e8b15ac39d43dea9ff7c2804534666782450e66
askrid <joonwoo3023@gmail.com>
2022-11-14 23:25:23 +0900

    web: setup mock service workers

---
+++ cago-web/src/mocks/handlers.ts
@@ -0,0 +1,44 @@
+import { rest } from "msw";
+import { profile, token, user } from "./stubs";
+
+// Simplest API handlers
+export const handlers = [
+  rest.post("/auth/login/", (req, res, ctx) => {
+    return res(
+      ctx.cookie("refresh_token", token.refresh),
+      ctx.json({
+        access: token.access,
+      })
+    );
+  }),
+
+  rest.post("/auth/signup/", async (req, res, ctx) => {
+    return res(
+      ctx.status(201),
+      ctx.json({
+        id: user.id,
+        email: user.email,
+      })
+    );
+  }),
+
+  rest.get("/auth/logout/", (req, res, ctx) => {
+    return res(ctx.status(204), ctx.cookie("refresh_token", ""));
+  }),
+
+  rest.post("/auth/refresh/", (req, res, ctx) => {
+    return res(
+      ctx.json({
+        access: token.access,
+      })
+    );
+  }),
+
+  rest.post("/customer-profiles/", (req, res, ctx) => {
+    return res(ctx.status(201), ctx.json(profile));
+  }),
+
+  rest.get("/customer-profiles/me/", (req, res, ctx) => {
+    return res(ctx.json(profile));
+  }),
+];
