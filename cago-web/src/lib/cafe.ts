26e36f517326a3bc4361bd7eff2f1bb8d567e357
jhm1313@naver.com <44698831+jhm1313@users.noreply.github.com>
2022-11-25 04:02:31 +0900

    web: toggle force-closed API

--- cago-web/src/lib/cafe.ts
+++ cago-web/src/lib/cafe.ts
@@ -1,6 +1,6 @@
 import axios, { AxiosError } from "axios";
 import { CagoAPIError, getCagoRequest } from "utils";
-import useSWR from "swr";
+import useSWR, { mutate } from "swr";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
 
@@ -37,6 +37,25 @@ export const registerCafe = async (
       const { errors } = error.response?.data!;
       if (errors.some((v) => v.code === "unique")) {
         throw Error("이미 등록된 카페입니다.");
+      }
+    }
+  }
+};
+
+export const toggleOpen = async (id: number, force_closed: boolean, token: string) => {
+  try {
+    const data = await getCagoRequest("patch", token)(`/cafes/${id}/`, {force_closed: force_closed});
+    await mutate(`/cafes/${id}`, data, { revalidate: false });
+  } catch (error) {
+    if (axios.isAxiosError<CagoAPIError>(error)) {
+      if (error.response?.status === 401) {
+        throw Error("로그인하지 않았습니다.");
+      }
+      if (error.response?.status === 403) {
+        throw Error("카페의 매니저가 아닙니다.");
+      }
+      if (error.response?.status === 404) {
+        throw Error("유효하지 않은 카페 ID입니다.");
       }
     }
   }
