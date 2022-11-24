0a3361a021662ce5814dfb88d81722e4d20f9dad
ImPJH <impjhy1129@naver.com>
2022-11-24 21:34:09 +0900

    web: create toggle function

--- cago-web/src/lib/dashboard.ts
+++ cago-web/src/lib/dashboard.ts
@@ -22,3 +22,15 @@ export const getCafeList = () => {
   })
   return { data }
 }
+
+export const phone_numberChanger = (phone_number: string | null) => {
+  return phone_number
+}
+
+export const setCafeClosed = (cafeId : number) => {
+  getCagoRequest('patch')(`/cafes/${cafeId}/`,{force_closed:true})
+}
+
+export const setCafeOpened = (cafeId : number) => {
+  getCagoRequest('patch')(`/cafes/${cafeId}/`,{force_closed:false})
+}
