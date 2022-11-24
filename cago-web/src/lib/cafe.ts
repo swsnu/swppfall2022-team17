c3ade1688b5167ef2b082cb880aaa98a6106a921
jhm1313@naver.com <44698831+jhm1313@users.noreply.github.com>
2022-11-25 01:36:17 +0900

    web: useCafe

--- cago-web/src/lib/cafe.ts
+++ cago-web/src/lib/cafe.ts
@@ -1,5 +1,14 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 import { CagoAPIError, getCagoRequest } from "utils";
+import useSWR from "swr";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+export interface Cafe {
+  id: number;
+  name: string;
+  force_closed: boolean;
+}
 
 export const registerCafe = async (
   registrationNumber: string,
@@ -31,4 +40,29 @@ export const registerCafe = async (
       }
     }
   }
+};
+
+export const useCafe = (admin: boolean) => {
+  const router = useRouter();
+  const path = router.pathname.split("/");
+  const detail = path.length > 3;
+  const cafeID = detail ? path[admin ? 3 : 2] : -1;
+  
+  // Fetch cafe only when the user at admin dashboard detail page.
+  const { data, error, mutate } = useSWR<Cafe, AxiosError>(
+    detail && `/cafes/${cafeID}/`,
+    getCagoRequest("get"),
+    { shouldRetryOnError: false }
+  );
+
+  const cafeSelected = !!data && !error;
+
+  useEffect(() => {
+    if (!detail) {
+      // Set data to undefined if user is not at the detail page.
+      mutate(undefined, { revalidate: false });
+    }
+  }, [detail, mutate]);
+
+  return { cafeSelected, cafe: data };
 };
