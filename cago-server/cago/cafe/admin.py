beb52087632f49728e5003e35ea61f02ca97fb08
askrid <joonwoo3023@gmail.com>
2022-11-29 15:41:32 +0900

    server: update models in cafe app

--- cago-server/cago/cafe/admin.py
+++ cago-server/cago/cafe/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Cafe, CafeImage, CafeMenu, CustomerProfile, ManagedCafe
+from .models import Cafe, CafeImage, CafeMenu, CafeReview, CustomerProfile, ManagedCafe
 
 admin.site.register(Cafe)
 admin.site.register(CafeImage)
 admin.site.register(CafeMenu)
+admin.site.register(CafeReview)
 admin.site.register(CustomerProfile)
 admin.site.register(ManagedCafe)
