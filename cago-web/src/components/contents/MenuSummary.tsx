9f6b5f62ec4675f7e28905f1175dd2832d3bb8ab
ImPJH <impjhy1129@naver.com>
2022-12-04 20:45:09 +0900

    web: add ReviewSummary with dummy data

--- cago-web/src/components/contents/MenuSummary.tsx
+++ cago-web/src/components/contents/MenuSummary.tsx
@@ -11,17 +11,6 @@ interface SummaryProps {
     cafe_id: string;
 }
 
-/* Individual Menu  */
-const MenuDetail = (menu: CafeMenuProps) => {
-    return (
-        <div className='shadow-lg m-2 w-full min-w-fit flex flex-col justify-center'>
-            <div>{menu.name}</div>
-            {menu.image && <img src={menu.image} className='h-44' />}
-            <div>{menu.price}원</div>
-        </div>
-    )
-}
-
 const MenuSummary = ({ cafe_id }: SummaryProps) => {
     const { mainMenuList } = useMenu(cafe_id)
     console.log(mainMenuList)
@@ -33,11 +22,16 @@ const MenuSummary = ({ cafe_id }: SummaryProps) => {
                         <>메뉴가 없습니다. 추가하려면 '자세히' 버튼을 눌러주세요.</>
                         :
                         <>
+                            <div className="mb-4">메뉴를 '추가' 및 '삭제' 하려면 '자세히' 버튼을 눌러주세요.</div>
                             <div className="contained mb-2">대표메뉴</div>
                             <div className="flex overflow-auto scrollbar-hide">
                                 {mainMenuList.map((mainMenu) => {
                                     return (
-                                        <MenuDetail key={`${mainMenu.id} main container`} id={mainMenu.id} name={mainMenu.name} price={mainMenu.price} image={mainMenu.image} />
+                                        <div key={`${mainMenu.id} main container`} className='shadow-lg m-2 w-full min-w-fit flex flex-col justify-center'>
+                                            <div>{mainMenu.name}</div>
+                                            {mainMenu.image && <img src={mainMenu.image} className='h-44' />}
+                                            <div>{mainMenu.price}원</div>
+                                        </div>
                                     )
                                 })}
                             </div>
