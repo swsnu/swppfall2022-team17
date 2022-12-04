799f11981e44294b5dd419531eaf7ae35719801c
ImPJH <impjhy1129@naver.com>
2022-12-04 18:53:12 +0900

    web: show menu based on backend data

--- cago-web/src/components/contents/MenuSummary.tsx
+++ cago-web/src/components/contents/MenuSummary.tsx
@@ -1,46 +1,48 @@
-import { getCagoRequest } from "utils"
-import temp_menu from "components/contents/temp_data/temp_menu.json"
+import { useMenu } from "lib/menu";
 
 interface CafeMenuProps {
     id: number;
     name: string;
-    price: string;
+    price: number;
     image?: string;
 }
 
+interface SummaryProps {
+    cafe_id: string;
+}
+
+/* Individual Menu  */
 const MenuDetail = (menu: CafeMenuProps) => {
     return (
-        <div key={`${menu.id}container`} className='min-w-fit flex flex-col justify-center'>
+        <div className='shadow-lg m-2 w-full min-w-fit flex flex-col justify-center'>
             <div>{menu.name}</div>
-            {menu.image && <img src={menu.image}></img>}
+            {menu.image && <img src={menu.image} className='h-44' />}
             <div>{menu.price}원</div>
         </div>
     )
 }
 
-const MenuSummary = () => {
+const MenuSummary = ({ cafe_id }: SummaryProps) => {
+    const { mainMenuList } = useMenu(cafe_id)
+    console.log(mainMenuList)
     return (
         <main className="flex flex-col">
-            <div className="outlined m-2 y-1/2">
-                <div className="contained">대표메뉴</div>
-                <div className="flex overflow-auto scrollbar-hide">
-                    {temp_menu.map((menu) => {
-                        return (
-                            menu.is_main &&
-                            <MenuDetail id={menu.id} name={menu.name} price={menu.price} image={menu.image}/>
-                        )
-                    })}
-                </div>
-            </div>
-            <div className="outlined m-2 y-1/2">
-                <div className="contained">전체메뉴</div>
-                <div className="grid grid-cols-3 gap-4">
-                    {temp_menu.map((menu) => {
-                        return (
-                            <MenuDetail id={menu.id} name={menu.name} price={menu.price} image={menu.image}/>
-                        )
-                    })}
-                </div>
+            <div className="m-2 y-1/2">
+                {
+                    mainMenuList.length == 0 ?
+                        <>메뉴가 없습니다. 추가하려면 '자세히' 버튼을 눌러주세요.</>
+                        :
+                        <>
+                            <div className="contained mb-2">대표메뉴</div>
+                            <div className="flex overflow-auto scrollbar-hide">
+                                {mainMenuList.map((mainMenu) => {
+                                    return (
+                                        <MenuDetail key={`${mainMenu.id} main container`} id={mainMenu.id} name={mainMenu.name} price={mainMenu.price} image={mainMenu.image} />
+                                    )
+                                })}
+                            </div>
+                        </>
+                }
             </div>
         </main>
     )
