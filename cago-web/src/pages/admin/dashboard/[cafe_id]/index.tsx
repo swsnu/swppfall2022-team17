3ae105e97f9528cb27d7e1b2402109f8bca27ae1
ImPJH <impjhy1129@naver.com>
2022-12-04 12:10:44 +0900

    web/ rebase main

--- cago-web/src/pages/admin/dashboard/[cafe_id]/index.tsx
+++ cago-web/src/pages/admin/dashboard/[cafe_id]/index.tsx
@@ -9,16 +9,21 @@ import { useRouter } from "next/router";
 import { NextPageWithLayout } from "pages/_app";
 import useSWR from "swr";
 import { getCagoRequest } from "utils";
+import BoardSummary from "components/contents/BoardSummary"
+import MenuSummary from "components/contents/MenuSummary"
+import ReviewSummary from "components/contents/ReviewSummary"
 
 interface CafeInfoBoxProps {
   title: string;
   children: React.ReactNode;
+  span?: string;
   path?: string;
 }
 
-const CafeInfoContainer = ({ title, children, path }: CafeInfoBoxProps) => {
+const CafeInfoContainer = ({ title, children, span, path }: CafeInfoBoxProps) => {
   return (
-    <div className="outlined w-full h-full flex flex-col px-4 py-3">
+    /* span for  */
+    <div className={`outlined w-full h-full flex flex-col px-4 py-3 col-span-${span}`}>
       <div className="flex justify-between items-center mb-4">
         <h2 className="text-lg">{title}</h2>
         {path && (
@@ -40,21 +45,21 @@ const DashboardDetail: NextPageWithLayout = () => {
   return (
     <main className="mt-16">
       {cafe && (
-        <div className="grid lg:grid-rows-3 lg:grid-cols-2 grid-rows-5 grid-flow-col gap-4">
+        <div className="mb-4 grid lg:grid-rows-3 lg:grid-cols-2 grid-rows-5 grid-flow-col gap-4">
           <CafeInfoContainer title="카페 사진" path={`/admin/dashboard/${cafe_id}/add-pictures`}>
-            {"TODO: picture"}
+            {/* <ImageView/> */}
           </CafeInfoContainer>
           <CafeInfoContainer title="카페 소개" path={`/admin/dashboard/${cafe_id}/info`}>
             {cafe.introduction ?? "카페 소개를 작성해보세요!"}
           </CafeInfoContainer>
-          <CafeInfoContainer title="공지사항" path={`/admin/dashboard/${cafe_id}/board`}>
-            {"TODO: board"}
+          <CafeInfoContainer title="공지사항" path={`/admin/dashboard/${cafe_id}/add-pictures`} span='2'>
+            <BoardSummary/>
           </CafeInfoContainer>
-          <CafeInfoContainer title="리뷰" path={`/admin/dashboard/${cafe_id}/reviews`}>
-            {"TODO: review"}
+          <CafeInfoContainer title="리뷰" path={`/admin/dashboard/${cafe_id}/add-pictures`}>
+            <ReviewSummary/>
           </CafeInfoContainer>
-          <CafeInfoContainer title="메뉴" path={`/admin/dashboard/${cafe_id}/menu`}>
-            {"TODO: menu"}
+          <CafeInfoContainer title="메뉴" path={`/admin/dashboard/${cafe_id}/add-pictures`}>
+            <MenuSummary/>
           </CafeInfoContainer>
         </div>
       )}
