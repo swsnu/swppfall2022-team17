059b7b195dac98c3089fb1d24fd554f4ee87878d
askrid <joonwoo3023@gmail.com>
2022-12-07 17:35:27 +0900

    web: review creation error handling

--- cago-web/src/lib/review.ts
+++ cago-web/src/lib/review.ts
@@ -1,6 +1,6 @@
-import { AxiosError } from "axios";
+import axios, { AxiosError } from "axios";
 import useSWR, { mutate } from "swr";
-import { getCagoRequest } from "utils";
+import { CagoAPIError, getCagoRequest } from "utils";
 import { Profile } from "./profile";
 
 export interface Review {
@@ -20,14 +20,27 @@ export const createReview = async (
   strength: string,
   token: string
 ) => {
-  await getCagoRequest<Review>("post", token)("/reviews/", {
-    cafe: cafe_id,
-    content: content,
-    rating: rating,
-    strength: strength,
-  });
-  mutate(`/reviews/?cafe_id=${cafe_id}`);
-  mutate(`/cafes/${cafe_id}/`);
+  try {
+    await getCagoRequest<Review>("post", token)("/reviews/", {
+      cafe: cafe_id,
+      content: content,
+      rating: rating,
+      strength: strength,
+    });
+    mutate(`/reviews/?cafe_id=${cafe_id}`);
+    mutate(`/cafes/${cafe_id}/`);
+  } catch (error) {
+    if (axios.isAxiosError<CagoAPIError>(error)) {
+      const { errors } = error.response?.data!;
+      if (errors.some((v) => v.code === "unique")) {
+        throw Error("해당 카페에 이미 리뷰를 작성했습니다.");
+      }
+
+      if (error.response?.status === 403) {
+        throw Error("해당 카페의 매니저는 리뷰를 작성할 수 없습니다.");
+      }
+    }
+  }
 };
 
 export const deleteReview = async (cafe_id: number, review_id: number, token: string) => {
