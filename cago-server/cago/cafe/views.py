046e85f2f2f8717c34f06dd2b313cd88cf9fcfa0
jhm1313@naver.com <44698831+jhm1313@users.noreply.github.com>
2022-12-07 12:55:41 +0900

    server: remove duplicated viewset

--- cago-server/cago/cafe/views.py
+++ cago-server/cago/cafe/views.py
@@ -206,24 +206,6 @@ class CafeReviewViewSet(
     serializer_class = CafeReviewSerializer
     permission_classes = [EditOwnerOnly, IsAuthenticatedOrReadOnly]
     filterset_fields = ["cafe_id"]
-
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
-
-
-class CafeReviewViewSet(
-    CreateModelMixin,
-    ListModelMixin,
-    DestroyModelMixin,
-    GenericViewSet,
-):
-    class EditOwnerOnly(BaseEditOwnerOnly):
-        owner_field = "author"
-
-    queryset = CafeReview.objects.all()
-    serializer_class = CafeReviewSerializer
-    permission_classes = [EditOwnerOnly, IsAuthenticatedOrReadOnly]
-    filterset_fields = ["cafe_id"]
     ordering = ["-created_at"]
 
     def perform_create(self, serializer):
