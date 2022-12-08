bc097c618302ade0d4347bd85ed3acf5aa6b9a47
askrid <joonwoo3023@gmail.com>
2022-12-09 03:33:16 +0900

    web: remove '통계'

--- cago-web/src/components/contents/StatisticsContainer.tsx
+++ cago-web/src/components/contents/StatisticsContainer.tsx
@@ -9,17 +9,14 @@ interface Props {
 
 const StatisticsContainer = ({ statList }: Props) => {
   return (
-    <>
-      <h2 className="text-lg font-semibold text-center mb-4">통계</h2>
-      <div className="flex flex-col sm:flex-row gap-2 justify-between flex-1 flex-shrink">
-        {statList.map((stat) => (
-          <div key={stat.name} className="bg-slate-50 shadow-lg text-center p-4 rounded-lg w-full">
-            <h3 className="font-semibold text-lg mb-2">{stat.name}</h3>
-            <p className="text-2xl font-bold">{stat.value}</p>
-          </div>
-        ))}
-      </div>
-    </>
+    <div className="flex flex-col sm:flex-row gap-2 justify-between flex-1 flex-shrink">
+      {statList.map((stat) => (
+        <div key={stat.name} className="bg-slate-50 shadow-lg text-center p-4 rounded-lg w-full">
+          <h3 className="font-semibold text-lg mb-2">{stat.name}</h3>
+          <p className="text-2xl font-bold">{stat.value}</p>
+        </div>
+      ))}
+    </div>
   );
 };
 
