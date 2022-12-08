28ebe5bec490dd8ab148f0aa522d8ebf9ec016f0
askrid <joonwoo3023@gmail.com>
2022-12-09 01:28:33 +0900

    server: change API url names

--- cago-server/cago/cafe/urls.py
+++ cago-server/cago/cafe/urls.py
@@ -16,9 +16,9 @@ cafe_router = routers.SimpleRouter()
 cafe_router.register(r"customer-profiles", CustomerProfileViewSet, "customer-profile")
 cafe_router.register(r"cafes", CafeViewSet, "cafe")
 cafe_router.register(r"menus", CafeMenuViewSet, "menu")
-cafe_router.register(r"board", CafeArticleViewSet, "board-article")
-cafe_router.register(r"comments", CafeCommentViewSet, "board-comment")
+cafe_router.register(r"board", CafeArticleViewSet, "article")
+cafe_router.register(r"comments", CafeCommentViewSet, "comment")
 cafe_router.register(r"cafe-images", CafeImageViewSet, "image")
-cafe_router.register(r"reviews", CafeReviewViewSet, "board-comment")
+cafe_router.register(r"reviews", CafeReviewViewSet, "review")
 
 urlpatterns = cafe_router.urls + [path("like/", CafeLikeAPIView.as_view(), name="like")]
