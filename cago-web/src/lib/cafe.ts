dfde87099ac0b7550a68fdaafac2ab85f5c754e9
askrid <joonwoo3023@gmail.com>
2022-12-09 05:03:42 +0900

    web: (fix) consider average rating null

--- cago-web/src/lib/cafe.ts
+++ cago-web/src/lib/cafe.ts
@@ -28,7 +28,7 @@ export interface ManagedCafe extends Omit<Cafe, "is_managed"> {
   num_taste: number;
   num_service: number;
   num_mood: number;
-  average_rating: number;
+  average_rating: number | null;
 }
 
 export const registerCafe = async (
